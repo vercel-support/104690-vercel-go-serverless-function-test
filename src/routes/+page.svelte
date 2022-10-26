6fdecfe08f9627b1afe61f3eef0ef498b2bce8e8
Jacob Adler <dev.ja1988@gmail.com>
2022-10-26 14:10:42 +0200

    some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <section>
 	<h1>Vercel serverless functions go example</h1>
-	<h3>Api endpoint</h3>
+	<h3>Api endpoint not working to build when deploying to production</h3>
+	<ul>
+		<li>Try: <a href="/api/test">api-link</a></li>
+		<li>Should return:</li>
+		<li>{'{ "value": "Hi from the api" }'}</li>
+		<li>works fine locally with 'vercel dev --listen 3000'</li>
+		<li>fails during deployment, since go doesn't recognize package 'models'</li>
+	</ul>
 </section>
 
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
